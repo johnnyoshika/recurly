18ebede61c3178cecc688dc941c9cd2881e0b455
Johnny Oshika <johnnyoshika@gmail.com>
2015-02-17 09:50:54 -0800

    Using Recurly.js' directly token method without DOM

--- app/pods/payment/route.js
+++ app/pods/payment/route.js
@@ -4,13 +4,13 @@ export default Ember.Route.extend({
 
     model: function() {
         return Ember.Object.create({
-            firstName: 'John',
-            lastName: 'Smith',
+            first_name: 'John',
+            last_name: 'Smith',
             address1: '7852 Foo St',
             city: 'Vancouver',
             country: 'Canada',
-            province: 'BC',
-            postalCode: 'V9J 9J9',
+            state: 'BC',
+            postal_code: 'V9J 9J9',
             number: '4111111111111111',
             month: '08',
             year: '16',
@@ -21,15 +21,18 @@ export default Ember.Route.extend({
     actions: {
         submit: function() {
 
-            // obviously using $ here is bad :-)
-            // this needs to be in a component
-            this.recurly.lib().token(Ember.$('#payment')[0], function(err, token) {
-                if (err) {
-                    console.log('error', err);
-                } else {
-                    console.log('token', token);
+            this.recurly.lib().token(
+                this.modelFor(this.routeName).getProperties(
+                    'first_name', 'last_name', 'address1', 'city', 'country', 'state', 'postal_code', 'number', 'month', 'year', 'cvv'
+                ), 
+                function(err, token) {
+                    if (err) {
+                        console.log('error', err);
+                    } else {
+                        console.log('token', token);
+                    }
                 }
-            });
+            );
 
         }
     }
